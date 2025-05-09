9bede2f38680f95c52925d8881bd8069511cd180
nakata5321 <nakata5321@users.noreply.github.com>
2025-05-09 13:49:09 +0000

    deploy: 831ccfd835d66dca0b76acfe315e690b076c0c03

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sensors.robonomics.network.nakata/sw.js', { scope: '/sensors.robonomics.network.nakata/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
